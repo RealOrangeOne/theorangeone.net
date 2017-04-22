cae45f9d1cee3315529147a81e6363644b75847a
Jake Howard <git@theorangeone.net>
2017-04-22 18:45:44 +0100

    Remove commented test code

--- static/src/js/alpha/main.js
+++ static/src/js/alpha/main.js
@@ -5,16 +5,6 @@
 */
 
 (function($) {
-
-	/*skel.breakpoints({
-		wide: '(max-width: 1680px)',
-		normal: '(max-width: 1280px)',
-		narrow: '(max-width: 980px)',
-		narrower: '(max-width: 840px)',
-		mobile: '(max-width: 736px)',
-		mobilep: '(max-width: 480px)'
-	});*/
-
 	$(function() {
 
 		var	$window = $(window),
@@ -24,14 +14,6 @@
 
 		// Fix: Placeholder polyfill.
 			$('form').placeholder();
-
-		// Prioritize "important" elements on narrower.
-			/*skel.on('+narrower -narrower', function() {
-				$.prioritize(
-					'.important\\28 narrower\\29',
-					skel.breakpoint('narrower').active
-				);
-			});*/
 
 		// Dropdowns.
 			$('#nav > ul').dropotron({
@@ -69,32 +51,11 @@
 					});
 
 			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
-				/*if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-					$('#navButton, #navPanel, #page-wrapper')
-						.css('transition', 'none');*/
 
 		// Header.
 		// If the header is using "alt" styling and #banner is present, use scrollwatch
 		// to revert it back to normal styling once the user scrolls past the banner.
 		// Note: This is disabled on mobile devices.
-			/*if (!skel.vars.mobile
-			&&	$header.hasClass('alt')
-			&&	$banner.length > 0) {
-
-				$window.on('load', function() {
-
-					$banner.scrollwatch({
-						delay:		0,
-						range:		0.5,
-						anchor:		'top',
-						on:			function() { $header.addClass('alt reveal'); },
-						off:		function() { $header.removeClass('alt'); }
-					});
-
-				});
-
-			}*/
-
 	});
 
 })(jQuery);
