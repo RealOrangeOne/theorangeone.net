080256df66735627d0f0baeb79f3d326baa6ba4b
Jake Howard <git@theorangeone.net>
2017-04-30 11:32:15 +0100

    Remove watching and cleanup build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,65 +1,49 @@
 const sass = require('node-sass');
 const fs = require('fs');
 const path = require('path');
-const watch = require('node-watch');
 const browserify = require('browserify');
-const livereload = require('livereload');
 const nsp = require('ncp').ncp;
 
-const STATIC = path.join(__dirname, '..', 'static')
+const STATIC = path.join(__dirname, '..', 'static');
+
+const SRC_DIR = path.join(STATIC, 'src');
+const BUILD_DIR = path.join(STATIC, 'build');
 
 const OUT_FILE = path.join(STATIC, 'build', 'css', 'style.css');
 const IN_FILE = path.join(STATIC, 'src', 'scss', 'style.scss');
 
 
-
 const b = browserify({
-  entries:[path.join(STATIC, 'src', 'js', 'index.js')],
+  entries: [path.join(SRC_DIR, 'js', 'index.js')],
   debug: true,
-})
-.transform({
-  global: true,
-  debug: true
-}, 'uglifyify');
+});
 
-const bundle = function () {
-  b.bundle(function () {
-    console.log('built js');
-  })
-  .on('error', console.log)
-  .pipe(fs.createWriteStream(path.join(STATIC, 'build', 'js', 'app.js')))
-}
+const SASS_OPTIONS = {
+  file: path.join(SRC_DIR, 'scss', 'style.scss'),
+  outFile: path.join(BUILD_DIR, 'css', 'style.css'),
+  sourceMap: true,
+  sourceMapEmbed: true,
+  watch: true,
+};
 
-
-const buildSCSS = function () {
-  sass.render({
-    file: IN_FILE,
-    outFile: OUT_FILE,
-    sourceMap: true,
-    sourceMapEmbed: true,
-    watch: true,
-  }, function (error, result) {
-    if (!error) {
-      fs.writeFileSync(OUT_FILE, result.css);
-      console.log("Build CSS");
-    } else {
-      console.log("ERROR", error);
-    }
-  });
-}
-
-nsp('node_modules/font-awesome/fonts', path.join(STATIC, 'build', 'fonts'), function (err) {
+nsp(path.join('node_modules', 'font-awesome', 'fonts'), path.join(BUILD_DIR, 'fonts'), function (err) {
   if (err) {
     return console.error(err);
   }
   console.log('Copied fonts');
 });
 
-bundle();
-buildSCSS();
-//
-// watch(path.join(STATIC, 'src', 'js'), {recursive: true}, bundle);
-// watch(path.join(STATIC, 'src', 'scss'), {recursive: true}, buildSCSS);
-//
-//
-// livereload.createServer().watch(path.join(STATIC, 'src'));
+b.bundle(function () {
+  console.log('built js');
+})
+.on('error', console.error)
+.pipe(fs.createWriteStream(path.join(BUILD_DIR, 'js', 'app.js')))
+
+
+sass.render(SASS_OPTIONS, function (error, result) {
+  if (error) {
+    return console.error("ERROR", error);
+  }
+    fs.writeFileSync(path.join(BUILD_DIR, 'css', 'style.css'), result.css);
+    console.log("Build CSS");
+});
