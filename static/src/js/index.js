6c28380529a0ad44062a4f176185ffa6477d55d9
Jake Howard <git@theorangeone.net>
2017-05-14 17:32:09 +0100

    'borrow' js from alpha too

--- static/src/js/index.js
+++ static/src/js/index.js
@@ -4,9 +4,7 @@ require('./jquery-global');
 require('bootstrap');
 
 // Install alpha things
-require('./alpha/jquery.dropotron.min');
-require('./alpha/util');
-require('./alpha/main');
+require('./alpha');
 
 
 $('.image').each(function () {  // setup div-image hybrids
