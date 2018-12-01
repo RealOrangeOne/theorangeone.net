04c4e6f0bcc1d3ce235886507dcf4b5582d59d9f
Jake Howard <git@theorangeone.net>
2018-12-01 20:33:25 +0000

    Update eslint config

--- static/src/js/index.js
+++ static/src/js/index.js
@@ -4,7 +4,7 @@ var Clipboard = require('clipboard');
 
 function waitFor(obj, property, callback) {
   // Wait for a property to exist on window before runnig callback
-  var intervalId = setInterval(function () {
+  var intervalId = setInterval(function() {
     if (obj.hasOwnProperty(property)) {
       clearInterval(intervalId);
       callback();
@@ -12,8 +12,8 @@ function waitFor(obj, property, callback) {
   }, 100);
 }
 
-
-$('.image').each(function () {  // setup div-image hybrids
+$('.image').each(function() {
+  // setup div-image hybrids
   var ele = $(this);
   if (ele.data('image')) {
     ele.css('background-image', 'url(' + ele.data('image') + ')');
@@ -22,41 +22,49 @@ $('.image').each(function () {  // setup div-image hybrids
   }
 });
 
-
-$(document).ready(function () {
-  waitFor($.fn, 'lightGallery', function () {
+$(document).ready(function() {
+  waitFor($.fn, 'lightGallery', function() {
     $('#light-gallery').lightGallery({
-      thumbnail:true,
+      thumbnail: true,
       animateThumb: false,
       showThumbByDefault: false,
       preload: 2,
-      download: false
+      download: false,
     });
   });
 
   // HACK: ToC has blank li if no initial header
-  $('#TableOfContents').each(function () {
+  $('#TableOfContents').each(function() {
     var ele = $(this);
     if (ele.find('a').length <= 3) {
       ele.parent().remove();
     } else if (ele.children('ul').children('li').length === 1) {
-      ele.children('ul').replaceWith(ele.children('ul').children('li').children('ul'));
+      ele.children('ul').replaceWith(
+        ele
+          .children('ul')
+          .children('li')
+          .children('ul')
+      );
     }
   });
 
-  waitFor(window, 'mermaid', function () {
+  waitFor(window, 'mermaid', function() {
     mermaid.initialize({
-      startOnLoad: true
+      startOnLoad: true,
     });
   });
-
 });
 
-$('.navbar-brand').on('click', function (event) {
+$('.navbar-brand').on('click', function(event) {
   if ($('html').scrollTop() > 100) {
-    $('html, body').stop().animate({
-      scrollTop: 0
-    }, 500);
+    $('html, body')
+      .stop()
+      .animate(
+        {
+          scrollTop: 0,
+        },
+        500
+      );
   } else {
     window.location = '/';
   }
@@ -64,12 +72,12 @@ $('.navbar-brand').on('click', function (event) {
 });
 
 var clipboard = new Clipboard('a');
-clipboard.on('success', function (e) {
+clipboard.on('success', function(e) {
   var ele = $(e.trigger);
   ele.find('i').attr('class', 'fas fa-check');
   alert('Copied "' + ele.data('clipboard-text') + '" to clipboard!');
 });
 
-$('[data-clipboard-text]').on('click', function (event) {
+$('[data-clipboard-text]').on('click', function(event) {
   event.preventDefault();
 });
