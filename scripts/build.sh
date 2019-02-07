c5a297f7951183b2a205b4c39e3e4a488addeff6
Jake Howard <git@theorangeone.net>
2019-02-07 19:28:19 +0000

    Add matrix well-known file

--- scripts/build.sh
+++ scripts/build.sh
@@ -26,5 +26,7 @@ hugo gen chromastyles --style=tango > $STATIC_BUILD/css/highlight.css
 
 hugo -vDEF --stepAnalysis --gc $HUGO_ARGS
 
-mkdir -p $OUTPUT_DIR/.well-known/
 cp $BASEDIR/static/keybase.txt $OUTPUT_DIR/keybase.txt
+
+mkdir -p $OUTPUT_DIR/.well-known/matrix
+cp $BASEDIR/static/matrix.json $OUTPUT_DIR/.well-known/matrix/server
