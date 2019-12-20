58d300f9913b964283e10b1f87807f7b7dc9775b
Jake Howard <git@theorangeone.net>
2019-12-20 09:22:49 +0000

    Remove the spellchecking
    Maintaining the .spelling file is nasty!

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,8 +4,9 @@ set -e
 
 source ./scripts/common.sh
 
+set -x
+
 sass-lint -vqc .sass-lint.yml
 eslint $STATIC_SRC/js
 yamllint data/*.yml
 yamllint config.yml
-mdspell --en-gb -ranx 'content/**/*.md'
